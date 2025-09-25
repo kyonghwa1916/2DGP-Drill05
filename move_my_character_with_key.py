bcfd93a85d7fda93107de54e65bb6c8e0b9a2f14
kyonghwa1916 - notebook <yujin0360@naver.com>
2025-09-25 20:11:20 +0900

    수업내용 기반 이도 구현, x할당 관련 문제 발생

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -6,15 +6,46 @@ backgound = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 running = True
 frame = 0
-dir = 0
+dirx = 0
+diry = 0
 
+def handle_event():
+    global running, dirx, diry
+    events = get_events()
+    for event in events:
+        if event.type == SDL_QUIT:
+            running = False
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dirx += 1
+            elif event.key == SDLK_LEFT:
+                dirx -= 1
+            elif event.key == SDLK_UP:
+                diry += 1
+            elif event.key == SDLK_DOWN:
+                diry -= 1
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dirx -= 1
+            elif event.key == SDLK_LEFT:
+                dirx += 1
+            elif event.key == SDLK_UP:
+                diry -= 1
+            elif event.key == SDLK_DOWN:
+                diry += 1
 
 
 while running:
+
+    x += dirx * 5
+    y += diry * 5
     clear_canvas()
     backgound.draw(400, 300)
-    character.clip_draw(frame * 100, 100, 100, 100, 400, 90)
+    character.clip_draw(frame * 100, 100, 100, 100, x, y)
     update_canvas()
+    handle_event()
     frame = (frame + 1) % 8
     delay(0.05)
 
