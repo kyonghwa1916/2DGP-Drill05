6f557f90a3e9bf9f313e4424dd284d1fee273b96
kyonghwa1916 - notebook <yujin0360@naver.com>
2025-09-25 19:16:16 +0900

    캐릭터시트 띄우기

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -5,11 +5,18 @@ open_canvas()
 backgound = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
 running = True
+frame = 0
+dir = 0
+
+
 
 while running:
     clear_canvas()
     backgound.draw(400, 300)
-
+    character.clip_draw(frame * 100, 100, 100, 100, 400, 90)
     update_canvas()
+    frame = (frame + 1) % 8
+    delay(0.05)
+
 
 close_canvas()
