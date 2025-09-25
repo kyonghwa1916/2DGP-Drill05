e88a53b4e9a4f9c2f4919ea59c8baa133bc1d325
kyonghwa1916 - notebook <yujin0360@naver.com>
2025-09-25 19:07:42 +0900

    배경 띄우기

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,6 +4,12 @@ open_canvas()
 
 backgound = load_image('TUK_GROUND.png')
 character = load_image('animation_sheet.png')
+running = True
 
+while running:
+    clear_canvas()
+    backgound.draw(400, 300)
+
+    update_canvas()
 
 close_canvas()
